62ef8c1c099ec1fdc989c738c3c4bbe2cf61f579
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2025-01-28 08:25:06 +0900

    bootstrapでスタイリング、HomePage仕上げ

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import './App.css';
 import BlogPage from './components/BlogPage';
 import Header from './components/Header';
 import HomePage from './components/HomePage';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
-  return <div className="App">
-    <Header />
-    <HomePage />
-    <BlogPage />
-  </div>
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+
+        <Routes>
+          <Route path='/' element={<HomePage />}></Route>
+          <Route path='/blog' element={<BlogPage />}></Route>
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
