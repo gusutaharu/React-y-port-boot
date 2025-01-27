c238ba0618b65012e6609a131bdc86f5c30ed49b
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2025-01-27 09:22:33 +0900

    HomePage,BlogPageコンポーネントの作成

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import BlogPage from './components/BlogPage';
 import Header from './components/Header';
+import HomePage from './components/HomePage';
 
 function App() {
   return <div className="App">
     <Header />
+    <HomePage />
+    <BlogPage />
   </div>
 }
 
