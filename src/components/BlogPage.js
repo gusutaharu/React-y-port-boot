c9693b7889a280c9b90a7d56bec3e1adfe92016e
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2025-01-28 08:37:05 +0900

    BlogPage.jsの仕上げ

--- src/components/BlogPage.js
+++ src/components/BlogPage.js
@@ -2,7 +2,22 @@ import React from 'react'
 
 const BlogPage = () => {
   return (
-    <div>BlogPage</div>
+    <div className='container'>
+      <h1>ブログ</h1>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ipsam temporibus voluptate aliquid, tempore reiciendis saepe maxime non eaque quibusdam minus qui eum animi inventore impedit veritatis assumenda dignissimos tempora enim quae! Nemo, consequuntur debitis ducimus soluta sint cum ea ullam veritatis accusamus cumque, culpa, autem aspernatur magnam eum vero animi. Commodi ab laboriosam facilis? Delectus temporibus dolor ad voluptate ratione id beatae tenetur eaque odit doloremque fuga neque amet autem explicabo eius, facere vitae suscipit? Voluptatibus vero modi, nam asperiores minus expedita repellendus quae mollitia amet molestias unde nemo hic incidunt, iste voluptates ab deleniti deserunt error aut molestiae
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ipsam temporibus voluptate aliquid, tempore reiciendis saepe maxime non eaque quibusdam minus qui eum animi inventore impedit veritatis assumenda dignissimos tempora enim quae! Nemo, consequuntur debitis ducimus soluta sint cum ea ullam veritatis accusamus cumque, culpa, autem aspernatur magnam eum vero animi. Commodi ab laboriosam facilis? Delectus temporibus dolor ad voluptate ratione id beatae tenetur eaque odit doloremque fuga neque amet autem explicabo eius, facere vitae suscipit? Voluptatibus vero modi, nam asperiores minus expedita repellendus quae mollitia amet molestias unde nemo hic incidunt, iste voluptates ab deleniti deserunt error aut molestiae
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ipsam temporibus voluptate aliquid, tempore reiciendis saepe maxime non eaque quibusdam minus qui eum animi inventore impedit veritatis assumenda dignissimos tempora enim quae! Nemo, consequuntur debitis ducimus soluta sint cum ea ullam veritatis accusamus cumque, culpa, autem aspernatur magnam eum vero animi. Commodi ab laboriosam facilis? Delectus temporibus dolor ad voluptate ratione id beatae tenetur eaque odit doloremque fuga neque amet autem explicabo eius, facere vitae suscipit? Voluptatibus vero modi, nam asperiores minus expedita repellendus quae mollitia amet molestias unde nemo hic incidunt, iste voluptates ab deleniti deserunt error aut molestiae
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ipsam temporibus voluptate aliquid, tempore reiciendis saepe maxime non eaque quibusdam minus qui eum animi inventore impedit veritatis assumenda dignissimos tempora enim quae! Nemo, consequuntur debitis ducimus soluta sint cum ea ullam veritatis accusamus cumque, culpa, autem aspernatur magnam eum vero animi. Commodi ab laboriosam facilis? Delectus temporibus dolor ad voluptate ratione id beatae tenetur eaque odit doloremque fuga neque amet autem explicabo eius, facere vitae suscipit? Voluptatibus vero modi, nam asperiores minus expedita repellendus quae mollitia amet molestias unde nemo hic incidunt, iste voluptates ab deleniti deserunt error aut molestiae
+      </p>
+    </div>
   )
 }
 
